e8f15c6c179c563282d1ec2c073e4d2e6344a975
clayec <longi@live.com>
2016-11-03 16:14:32 +0100

    step 2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+function onSendChanged(){
+  var contactName = document.querySelector('.send').value.trim();
+  var values = {n: contactName};
+  cozysdk.create('Contact', values, function(err, res) {
+    if (err != null) return alert(err);
+    // res.id == "8239283928326"
+    document.querySelector('.send').value = '';
+    updateContactList();
+  });
+}
+
+
 function updateContactList(){
   cozysdk.defineRequest('Contact', 'all', 'function(doc) { emit(doc.n); }', function(err, res) {
     if (err != null) return alert(err);
@@ -27,5 +39,6 @@ function render(contacts) {
 	document.querySelector('.contact-list').innerHTML = HTML;
 }
 
+document.querySelector('.send').addEventListener('change', onSendChanged);
 document.addEventListener("DOMContentLoaded", updateContactList);
 

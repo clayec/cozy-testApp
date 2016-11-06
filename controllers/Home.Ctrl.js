f70af0a1c2743e1182657fa89c657747e1ec831b
clayec <longi@live.com>
2016-11-06 23:31:43 +0100

    bug solved

--- controllers/Home.Ctrl.js
+++ controllers/Home.Ctrl.js
@@ -1,10 +1,9 @@
 angular.module('browserapp').controller('HomeAngCtrl', HomeAngCtrl);
 
-HomeAngCtrl.$inject = ['CozySdk'];
 
 function HomeAngCtrl() {
     var vm = this;
 
-    vm.hello = "world";
+    vm.hello = "worldaas";
 
 }
